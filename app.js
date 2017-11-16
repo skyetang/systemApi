36b8c46ac91967975e90bea5a7799bda627ef82d
skye <forbetter@yeah.net>
2017-11-17 00:03:49 +0800

    singup set.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const login = require('./routes/login');
 
 require('./config/passport')(passport);
@@ -9,13 +10,12 @@ require('./config/passport')(passport);
 const app = express();
 
 const db = mongoose.connect('mongodb://localhost:27017/test', { useMongoClient:true });
-db.on('error', (error) => {
-    console.log(`数据库连接失败：${error}`);
-});
-db.on('open', () => {
-    console.log('——数据库连接成功！——');
-});
+mongoose.Promise = global.Promise;
 
+app.use(session({
+    secret: 'skye tang',
+    cookie:{ maxAge:180*60*1000 } //store保存时间
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
@@ -23,10 +23,17 @@ app.use('/login', login);
 
 
 const port = process.env.PORT || 8858;
-
 console.log(`> Starting dev server on port: ${port}`);
 
 app.listen(port, () => {
     console.log('——服务启动成功！——');
+});
+
+db.on('error', (error) => {
+    console.log(`数据库连接失败：${error}`);
+});
+
+db.on('open', () => {
+    console.log('——数据库连接成功！——');
 });
 
--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const Router = express.Router();
 const passport = require('passport');
+const User = require('../model/user');
 
-Router.get('/test', (req, res) => {
-    res.send({
-        message: 'success'
-    })
-});
-
-Router.post('/test', passport.authenticate('local', { session: false }), (req, res) => {
-    console.log(req.user);
+Router.post('/singin', passport.authenticate('local'), (req, res) => {
     res.send({ message: 'validate success' });
 });
+
+Router.post('/register', (req, res) => {
+    let pwd = req.body.password;
+    /* bcrypt.hash(pwd, 10, (err, hash) => {
+        if(err) return err;
+        pwd = hash;
+    }); */
+    const user = new User({
+        username: req.body.username,
+        password: pwd
+    });
+    user.save((err, user) => {
+        if(err) {
+            res.send({ 'err': err, 'success': false })
+        } else {
+            res.send({ 'err': err, 'success': true })
+        }
+    });
+});
+
 
 Router.post('/test2', (req, res) => {
         passport.authenticate('local', {
