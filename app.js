0669fffe8ae687cd0430727516bba086400b7e7b
skye <forbetter@yeah.net>
2017-12-04 23:59:14 +0800

    edit to koa.

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-const express = require('express');
+const koa = require('koa');
+const Router = require('koa-router');
 const passport = require('passport');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const bodyParser = require('koa-bodyparser');
 const login = require('./routes/login');
 
 require('./config/passport')(passport);
 
-const app = express();
+const app = new koa();
+const router = new Router();
 
 const db = mongoose.connect('mongodb://localhost:27017/test', { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+router.use('/login', login.routes());
+
+app.use(bodyParser());
 app.use(passport.initialize());
-app.use('/login', login);
+app.use(router.routes());
 
 const port = process.env.PORT || 8858;
 console.log(`> Starting dev server on port: ${port}`);
--- routes/login.js
+++ routes/login.js
@@ -1,24 +1,25 @@
-const express = require('express');
-const Router = express.Router();
+const Router = require('koa-router');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 const User = require('../model/user');
 const isAuth = require('../config/auth');
-
+const router = new Router();
 /*
 * 如果不需要passport自动通过session保存登录状态及设置cookie，则在验证时通过session:false
 * 如果需要用到，则需要引入cook-parser,express-session, 并且user passport.session
 * 设置serializeUser,deserializeUser方法等
 * */
-Router.post('/singin', passport.authenticate('local', { session: false }), (req, res) => {
-  res.send(req.user);
+router.post('/singin', (ctx) => {
+  return passport.authenticate('local', { session: false }, (req, res) => {
+    console.log(req, res);
+  })(ctx);
 });
 
-Router.post('/check', isAuth, (req, res) => {
+router.post('/check', isAuth, (req, res) => {
   res.send({ success: true, message: '检测成功' });
 });
 
-Router.post('/register', (req, res) => {
+router.post('/register', (req, res) => {
   bcrypt.hash(req.body.password, 10, (err, hash) => {
     if (err) return err;
     const user = new User({
@@ -35,7 +36,7 @@ Router.post('/register', (req, res) => {
   });
 });
 
-Router.post('/test2', (req, res) => {
+router.post('/test2', (req, res) => {
   passport.authenticate('local', {
     session: false,
     successRedirect: '/',
@@ -43,4 +44,4 @@ Router.post('/test2', (req, res) => {
   })(req, res);
 });
 
-module.exports = Router;
+module.exports = router;
